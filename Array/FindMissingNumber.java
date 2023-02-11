07a6b5096691b46b2f6223eaaf31cfd7a3c2d4c2
Divyanshu Padalia <63751330+prideofscpadalia@users.noreply.github.com>
2023-02-11 11:46:31 +0000

    Pending changes exported from your codespace

---
+++ Array/FindMissingNumber.java
@@ -0,0 +1,12 @@
+class FindMissingNumber{
+    public static void main(String [] args){
+        int arr[] = new int [100];
+        for(int i= 0;i<arr.length;i++){
+            arr[i]=i+1;
+            int Expctdsum = 0;
+            Expctdsum = Expctssum + arr[i];
+            
+        }
+        System.out.println(Expctdsum);
+    }
+}
